77a1f3cd9e5cd56743eb2370e87e2e89403b5a42
Guilherme Sequeira <gsequeira@adminisrsimac14.americas.media.global.loc>
2016-08-16 14:47:28 -0300

    ajuste lint

--- gulpfile.js
+++ gulpfile.js
@@ -22,104 +22,12 @@ gulp.task('minify', function() {
 var csslintErrorCount = {};
 
 gulp.task('scss-lint', function() {
-  var gutil = require('gulp-util');
-  var errors = {
-      'known-properties' : 0,
-      //'display-property-grouping',
-      //'compatible-vendor-prefixes',
-      'regex-selectors' : 0,
-      'duplicate-background-images' : 2,
-      'duplicate-properties' : 0,
-      'empty-rules' : 0,
-      'import' : 0,
-      'selector-max' : 5000000,
-      'universal-selector' : 3,
-      'unqualified-attributes' : 0,
-      'vendor-prefix' : 0
-  };
-
-  var customReporter = function() {
-    return through.obj(function(file, enc, cb) {
-      var foundErrors = [];
-      //gutil.log(gutil.colors.cyan(file.csslint.errorCount)+' errors in '+gutil.colors.magenta(file.path));
-
-      var reportFile = file.csslint || 0;
-
-      if (reportFile) {
-
-        if(!file.csslint.results) {
-          gutil.log(gutil.colors.cyan(' No results for '+gutil.colors.magenta(file.path)));
-          return cb(null, file);
-        }
-
-        file.csslint.results.forEach(function(result) {
-          var isError = false;
-          _.forOwn(errors, function(c, e){
-            if(result.error.rule.id === e)
-            {
-              var count = csslintErrorCount[e];
-              if(!count){
-                count = 1;
-              }
-              else{
-                count++;
-              }
-              csslintErrorCount[e] = count;
-              foundErrors.push(result);
-            }
-          });
-        });
-
-        gutil.log(gutil.colors.cyan(foundErrors.length) +
-        ' filtered errors in '+gutil.colors.magenta(file.path));
-        foundErrors.forEach(function(result) {
-          gutil.log(result.error.rule.id + ' - ' + result.error.message +
-          ' on line '+result.error.line);
-          gutil.log(gutil.colors.yellow(result.error.evidence));
-        });
-
-        //Should break build because of errors?
-
-      }
-
-      var shouldBreakBuild = false;
-
-      if(foundErrors.length){
-        _.forOwn(errors,function(c, e){
-          if(csslintErrorCount[e] > c){
-            gutil.log(gutil.colors.red(e + ' - ' + csslintErrorCount[e] +
-            ' errors. Exceeds global project accepted count (' + c + ').'));
-            shouldBreakBuild = true;
-          }
-          else{
-            if(csslintErrorCount[e]){
-                gutil.log(gutil.colors.magenta(e + ' - ' +
-                csslintErrorCount[e] +
-                ' errors. Under global accepted count (' + c + ').'));
-            }
-            //shouldBreakBuild = true;
-          }
-        });
-      }
-
-      if(shouldBreakBuild){
-        gutil.log(gutil.colors.red('--------------'));
-        gutil.log(gutil.colors.red('csslint failed'));
-        gutil.log(gutil.colors.red('--------------'));
-        return cb(new gutil.PluginError('csslint', 'CSSLint failed for ' +
-        file.relative), file);
-      }
-      return cb(null, file);
-    });
-  };
-
-  csslintErrorCount = {};
   return gulp.src('src/app/scss/*.scss')
     .pipe(scsslint({
-      config: './.scss-lint.yml'
+      'reporterOutputFormat': 'Checkstyle',
+      'filePipeOutput': 'scssReport.xml'
     }))
-    .pipe(customReporter());
-    // .pipe(scsslint.failReporter());
+    .pipe(gulp.dest('./reports'));
 });
 
 
